e980da330e659fd7d85ace882d1a22acf85d5d52
Shehab Taher <shehab.taher93@gmail.com>
2023-05-28 03:40:40 +0300

    First commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,109 @@
+import Link from "next/link";
+import React from "react";
+import Logo from "./Logo";
+import { useRouter } from "next/router";
+import {
+  DribbbleIcon,
+  FacebookIcon,
+  GithubIcon,
+  LinkedInIcon,
+  MoonIcon,
+  PinterestIcon,
+  SunIcon,
+  TwitterIcon,
+} from "./Icon";
+import { motion } from "framer-motion";
+import useThemeSwitcher from "./hooks/useThemeSwitcher";
+
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span
+        className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5
+      group-hover:w-full translate-[width] ease duration-300 dark:bg-light
+      ${router.asPath === href ? "w-full" : "w-0"}
+      `}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
+
+const NavBar = () => {
+
+const [mode,setMode] = useThemeSwitcher()
+
+  return (
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light">
+      <nav>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
+        <CustomLink href="/articles" title="Articles" className="ml-4" />
+      </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a
+          href="https://twitter.com/ShehabTaher_"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3"
+        >
+          <TwitterIcon />
+        </motion.a>
+        <motion.a
+          href="https://github.com/ShehabTaher"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3"
+        >
+          <GithubIcon />
+        </motion.a>
+        <motion.a
+          href="https://www.linkedin.com/in/shehab-taher/"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3"
+        >
+          <LinkedInIcon />
+        </motion.a>
+        {/* <a href="/" target={"_blank"}>
+          <DribbbleIcon />
+        </a> */}
+        {/* <a href="/" target={"_blank"}>
+          <PinterestIcon />
+        </a> */}
+        <motion.a
+          href="https://www.facebook.com/ShehabElDenTaher"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 ml-3"
+        >
+          <FacebookIcon />
+        </motion.a>
+
+        <button
+        onClick={()=> setMode(mode === "light"? "dark":"light")}
+        className={`ml-3 flex items-center justify-center rounded-full p-1 
+        ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}>
+        {
+          mode === "dark" ?
+          <SunIcon className={"fill-dark"} />
+          : <MoonIcon className={"fill-dark"} />
+        }
+        </button>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
---
+++ src/components/footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Layout from './Layout'
+import Link from 'next/link'
+
+const footer = () => {
+  return (
+    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg
+    dark:text-light dark:border-light'
+    >
+        <Layout className='py-8 flex items-center justify-between'>
+            <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
+            <div className='flex items-center'>
+            Build with <span className='text-primary dark:text-primaryDark text-2xl px-1'>&#9825;</span> by&nbsp; <Link href="https://www.linkedin.com/in/shehab-taher/" target={"_blank"} className='underline underline-offset-2'>Shehab Taher</Link>
+            </div>
+            <Link href="https://github.com/ShehabTaher" target={"_blank"}>Say Hello</Link>
+        </Layout>
+    </footer>
+  )
+}
+
+export default footer
